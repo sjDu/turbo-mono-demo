391e08ea50a0ccbe48c9c4ca30a2ad6a70aa98b0
James Du <james.du@mobisocial.us>
2024-05-31 11:51:08 +0800

    chore: add github action

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui", "@repo/github-service"],
+  basePath: "/turbo-mono-demo",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
